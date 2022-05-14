2afd6bd97db46e40e064a049f0c5ba4d9d2a5e4a
Александр Гетманов <getmanov.wyrik@yandex.ru>
2022-05-14 06:38:47 +0300

    for 1 review v2 14/05/2022 PIP 8

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -36,12 +36,12 @@ class PostPagesTests(TestCase):
         cls.image = SimpleUploadedFile(
             name='small.gif',
             content=(
-             b'\x47\x49\x46\x38\x39\x61\x02\x00'
-             b'\x01\x00\x80\x00\x00\x00\x00\x00'
-             b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
-             b'\x00\x00\x00\x2C\x00\x00\x00\x00'
-             b'\x02\x00\x01\x00\x00\x02\x02\x0C'
-             b'\x0A\x00\x3B'
+                b'\x47\x49\x46\x38\x39\x61\x02\x00'
+                b'\x01\x00\x80\x00\x00\x00\x00\x00'
+                b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+                b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+                b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+                b'\x0A\x00\x3B'
             ),
             content_type='image/gif'
         )
@@ -51,7 +51,6 @@ class PostPagesTests(TestCase):
             group=cls.group,
             image=cls.image,
         )
-        cls
 
     @classmethod
     def tearDownClass(cls):
